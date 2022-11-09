8c08ac063b753734a2f484f5c161d09897c119fa
carrascochristhoperbiotech <117690680+carrascochristhoperbiotech@users.noreply.github.com>
2022-11-09 12:09:15 -0500

    Se ha entregado la tarea tc2_CarrascoChristhoper
    Se realizó un bash escript para ver las 7 primeras filas y las 5 ultimas filas con uin mensaje de separacion entre ellas. Ademas, se creo un segundo bash ecrip en el cual se seleccionó de la columna "id" los registros que teniman en 5, se ordenó por la columna betweennes y el resultado se lo regsitro en un archivo csv

---
+++ tc2_CarrascoChristhoper/tc2_CarrascoChristhoper.sh
@@ -0,0 +1,5 @@
+head -n 7 EigenBetw.tsv 
+echo "Ultimas filas"
+tail -n 5 EigenBetw.tsv
+
+ 
---
+++ tc2_CarrascoChristhoper/tc2_CarrascoChristhoper2.sh
@@ -0,0 +1,5 @@
+
+#Seleccion de registros que terminan 
+grep AGAP0[1-9]*[5], EigenBetw.tsv  |sort -t, -k3 -n >EigenBetw5.csv
+
+
