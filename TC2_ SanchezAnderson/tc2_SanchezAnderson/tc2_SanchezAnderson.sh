42a70210813ab1461c2a7f94c3395204a64827df
Anderson Fabian Sanchez Pujota <117690512+SanchezAnderson31@users.noreply.github.com>
2022-11-09 17:39:27 -0500

    Carpeta que contiene los archivos
    Modificado por Anderson Sánchez

---
+++ TC2_ SanchezAnderson/tc2_SanchezAnderson/tc2_SanchezAnderson.sh
@@ -0,0 +1,7 @@
+echo "Inicio de las 7 primeras lineas"
+cat EigenBetw.tsv | head -n 7
+echo "Terminacion 7 primeras lineas"
+echo "Inicio de las 5 ultimas lineas"
+cat EigenBetw.tsv | tail -n 5
+echo "Terminacion 5 ultimas lineas"
+
---
+++ TC2_ SanchezAnderson/tc2_SanchezAnderson/tc2_SanchezAnderson2.sh
@@ -0,0 +1,1 @@
+cat EigenBetw.tsv | grep AGAP[0-9]*5, | sort -r -n -t "." -k 3 > EigenBetw5.csv
